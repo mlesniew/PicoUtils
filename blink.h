9041c1692f4d7c48c7f864b20e4027b92d0cacaf
Michał Leśniewski <mlesniew@gmail.com>
2021-02-17 19:44:01 +0100

    Add blink.h

---
+++ blink.h
@@ -0,0 +1,91 @@
+#ifndef ARDUINO_UTILS_BLINK_H
+#define ARDUINO_UTILS_BLINK_H
+
+#include <Arduino.h>
+#include <Ticker.h>
+
+#include "stopwatch.h"
+
+class Blink {
+    public:
+        Blink(unsigned long pattern = 0b10, unsigned long interval = 500)
+            : interval(interval), pattern(pattern) {
+            restart_pattern();
+            next();
+        }
+
+        void set_pattern(unsigned long pattern) {
+            if (this->pattern != pattern) {
+                this->pattern = pattern;
+                restart_pattern();
+            }
+        }
+
+        unsigned long get_pattern() const {
+            return this->pattern;
+        }
+
+        void tick() {
+            if (stopwatch.elapsed_millis() > interval) {
+                next();
+                stopwatch.reset();
+            }
+        }
+
+        void restart_pattern() {
+            for (position = 31; position > 0; --position) {
+                if ((pattern >> position) & 1)
+                    break;
+            }
+        }
+
+        const unsigned long interval;
+
+    protected:
+        virtual void set(bool state) = 0;
+
+    private:
+        void next() {
+            bool led_on = (pattern >> position) & 1;
+            set(led_on);
+            if (position-- == 0)
+                restart_pattern();
+        }
+
+        Stopwatch stopwatch;
+        unsigned long pattern;
+        unsigned char position;
+};
+
+
+class BlinkOutput : public Blink {
+    public:
+        BlinkOutput(unsigned int pin, unsigned long pattern = 0b10, unsigned long interval = 500, bool inverted = false)
+            : Blink(pattern, interval), pin(pin), inverted(inverted) {
+            pinMode(pin, OUTPUT);
+        }
+
+    protected:
+        void set(bool state) override {
+            digitalWrite(pin, (state != inverted) ? HIGH : LOW);
+        }
+
+        const unsigned int pin;
+        const bool inverted;
+};
+
+class BackgroundBlinker {
+    public:
+        BackgroundBlinker(Blink & blink) : blink(blink) {
+            ticker.attach(float(blink.interval) * 0.001, [&blink] { blink.tick(); });
+        }
+        ~BackgroundBlinker() {
+            ticker.detach();
+        }
+
+    private:
+        Blink & blink;
+        Ticker ticker;
+};
+
+#endif
