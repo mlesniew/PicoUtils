1831ce7686bd519f751521bcc265d31543af9c58
Michał Leśniewski <mlesniew@gmail.com>
2023-10-05 22:03:31 +0200

    Make Watch handle nan correctly

--- src/PicoUtils/watch.h
+++ src/PicoUtils/watch.h
@@ -8,38 +8,52 @@ template <typename T>
 class Watch {
     public:
         Watch(std::function<T()> getter, std::function<void(T)> callback):
-            callback(callback), getter(getter), last_value(T(getter())) {
+            callback(callback), getter(getter), old_value(T(getter())) {
         }
 
         Watch(std::function<T()> getter, std::function<void()> change_callback):
-            callback([change_callback](T) { change_callback(); }), getter(getter), last_value(T(getter())) {
-        }
-
-        void tick() {
-            T new_value = getter();
-            if (new_value != last_value) {
-                callback(new_value);
-                last_value = new_value;
-            }
+            callback([change_callback](T) { change_callback(); }), getter(getter), old_value(T(getter())) {
         }
 
         void tick() const {
             T new_value = getter();
-            if (new_value != last_value) {
+            if (new_value != old_value) {
                 callback(new_value);
-                last_value = new_value;
+                old_value = new_value;
             }
         }
 
         void fire() const {
-            last_value = getter();
-            callback(last_value);
+            old_value = getter();
+            callback(old_value);
         }
 
     protected:
         std::function<void(T)> callback;
         std::function<T()> getter;
-        mutable T last_value;
+        mutable T old_value;
 };
+
+template <>
+inline void Watch<double>::tick() const {
+    double new_value = getter();
+    const bool old_nan = std::isnan(old_value);
+    const bool new_nan = std::isnan(new_value);
+    if ((old_nan != new_nan) || (!old_nan && !new_nan && (new_value != old_value))) {
+        callback(new_value);
+        old_value = new_value;
+    }
+}
+
+template <>
+inline void Watch<float>::tick() const {
+    float new_value = getter();
+    const bool old_nan = std::isnan(old_value);
+    const bool new_nan = std::isnan(new_value);
+    if ((old_nan != new_nan) || (!old_nan && !new_nan && (new_value != old_value))) {
+        callback(new_value);
+        old_value = new_value;
+    }
+}
 
 }
